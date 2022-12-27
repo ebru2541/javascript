aa78337c25066d0c2c946a8fb2d6b5c2e87b4e47
Rose <ebrue7949@gmail.com>
2022-12-27 15:57:08 +0300

    problemler

--- problemler/problemler1.js
+++ problemler/problemler1.js
@@ -1,16 +1,16 @@
-//? girilen sayı 20 den küçük ise aradaki farkın karesini 20 den büyükse aynı sayıyı ekrana yazdır
+// //? girilen sayı 20 den küçük ise aradaki farkın karesini 20 den büyükse aynı sayıyı ekrana yazdır
 
-function fark(sayi) {
-  if (sayi > 20) {
-    return (sayi - 20) * 2;
-  } else {
-    return sayi;
-  }
-}
-console.log(fark(15));
-console.log(fark(30));
+// function fark(sayi) {
+//   if (sayi > 20) {
+//     return (sayi - 20) * 2;
+//   } else {
+//     return sayi;
+//   }
+// }
+// console.log(fark(15));
+// console.log(fark(30));
 
-// //? iki sayı girişi al bunlardan hangisinin büyük old bulan fonk yaz.
+// // //? iki sayı girişi al bunlardan hangisinin büyük old bulan fonk yaz.
 // function big(a, b) {
 //   if (a > b) {
 //     return `${a}, ${b} den büyüktür.`;
@@ -20,7 +20,7 @@ console.log(fark(30));
 // }
 // console.log(big(8, 5));
 
-//? 100 ile 1000 arasında 3 ve 5 bolünen sayıları yazın
+// //? 100 ile 1000 arasında 3 ve 5 bolünen sayıları yazın
 
 // const kat = () => {
 //   // let arr = [];
@@ -33,27 +33,28 @@ console.log(fark(30));
 //   }
 //   return str;
 // };
-// // const division = () => {
-// //   let arr = "";
-// //   let say;
-// //   for (let i = 100; i <= 1000; i++) {
-// //     if (i % 15 == 0) {
-// //       arr += i + ", ";
-// //     }
-// //   }
-// //   return arr;
-// // };
-// // console.log(division());
 
-// console.log(kat());
+// //! 2.yol
+// const division = () => {
+//   let arr = "";
+//   let say;
+//   for (let i = 100; i <= 1000; i++) {
+//     if (i % 15 == 0) {
+//       arr += i + ", ";
+//     }
+//   }
+//   return arr;
+// };
+// console.log(division());
 
-//? üçgenin alanını bulun
+
+// //? üçgenin alanını bulun
 // let a = prompt("üçgenin taban uzunluğunu yazın:");
-// // let h = prompt("üçgenin yüksekliğini yazın:");
+// let h = prompt("üçgenin yüksekliğini yazın:");
 // let alan = a * h * 0.5;
 // console.log(alan);
 
-//? girilen ayın kaç gün çektiğini ekrana yazdırın
+// //? girilen ayın kaç gün çektiğini ekrana yazdırın
 
 // let monthUserInput = (month) => {
 //   let month1 = month.toLowerCase();
@@ -83,14 +84,14 @@ console.log(fark(30));
 
 // console.log(monthUserInput("april"));
 
-//? kısa ve uzun kenarı verilen dikdörtgenin çevresini ve alanını
+// //? kısa ve uzun kenarı verilen dikdörtgenin çevresini ve alanını
 
 // function dikDotrgen(x, y) {
 //   return `alan=${x * y}, çevre=${2(x + y)}`;
 // }
 // console.log(dikDotrgen(20, 15));
 
-//? dairenin alanını bulun
+// //? dairenin alanını bulun
 // function daire(r){
 //   let pi=3.14
 //   let alan= pi * r * r
@@ -102,64 +103,116 @@ console.log(fark(30));
 // `alan=${alan}, cevre=${cevre}`;
 
 
-//! Clarusway'deki haftalik ders ve etkinlik programinizi,
-//! console'dan girilen gun degerine gore cikti veren kodu
-//! switch-case yapisi ile yaziniz.
+// //! Clarusway'deki haftalik ders ve etkinlik programinizi,
+// //! console'dan girilen gun degerine gore cikti veren kodu
+// //! switch-case yapisi ile yaziniz.
 
-//! Program
-//! Pazartesi, Sali ,Çarsamba, Persembe -> InClass
-//! Cuma -> Teamwork
-//! Cumartesi ->  InClass + Workshop
-//! Pazar -> Self-Study
-//! Aksi takdirde -> Yanlis gun girildi.
+// //! Program
+// //! Pazartesi, Sali ,Çarsamba, Persembe -> InClass
+// //! Cuma -> Teamwork
+// //! Cumartesi ->  InClass + Workshop
+// //! Pazar -> Self-Study
+// //! Aksi takdirde -> Yanlis gun girildi.
 
-      const day = prompt("Programini istediginiz gunu giriniz:");
+//       const day = prompt("Programini istediginiz gunu giriniz:");
 
-      let program;
+//       let program;
 
-      switch (day) {
-        case "pazartesi":
-        case "sali":
-        case "carsamba":
-        case "persembe":
-          program = "InClass";
-          break;
-        case "cuma":
-          program = "Teamwork";
-          break;
-        case "cumartesi":
-          program = "InClass + Workshop";
-          break;
-        case "pazar":
-          program = "Self-Study";
-          break;
+//       switch (day) {
+//         case "pazartesi":
+//         case "sali":
+//         case "carsamba":
+//         case "persembe":
+//           program = "InClass";
+//           break;
+//         case "cuma":
+//           program = "Teamwork";
+//           break;
+//         case "cumartesi":
+//           program = "InClass + Workshop";
+//           break;
+//         case "pazar":
+//           program = "Self-Study";
+//           break;
 
-        default:
-          alert("Yanlis gun girildi.");
-          break;
-      }
-      console.log(`Bugunku programiniz: ${program}`);
+//         default:
+//           alert("Yanlis gun girildi.");
+//           break;
+//       }
+//       console.log(`Bugunku programiniz: ${program}`);
 
-      //? Dort Islem Hesap Makinasi (2 Sayı bir operator, if-elseif)
-function calculator(a, b, islem) {
-  if (islem === "+") {
-    return a + b;
-  } else if (islem === "-") {
-    return a - b;
-  } else if (islem === "*") {
-    return a * b;
-  } else if (islem === "/") {
-    return a / b;
-  }
-  else{
-    return `+, *, *, / işlemlerinden birini seçiniz`
-  }
-}
-console.log(calculator(2, 4, "+"));
+//  //? Dort Islem Hesap Makinasi (2 Sayı bir operator, if-elseif)
+// function calculator(a, b, islem) {
+//   if (islem === "+") {
+//     return a + b;
+//   } else if (islem === "-") {
+//     return a - b;
+//   } else if (islem === "*") {
+//     return a * b;
+//   } else if (islem === "/") {
+//     return a / b;
+//   }
+//   else{
+//     return `+, *, *, / işlemlerinden birini seçiniz`
+//   }
+// }
+// console.log(calculator(2, 4, "+"));
 
-//?Maasi asgari ucretten az olanlara %50 zam,fazla olanlara ise %10 zam yapmak istiyoruz.
+// //?Maasi asgari ucretten az olanlara %50 zam,fazla olanlara ise %10 zam yapmak istiyoruz.
 
-let maas=prompt("maaşınızı yazınız: ")
-let asgariUcret=5500
-let maasHes= maas > asgariUcret ? maas*1.1 : maas*1.5
-console.log( `yeni maaşınız: ${maasHes}`);
+// let maas=prompt("maaşınızı yazınız: ")
+// let asgariUcret=5500
+// let maasHes= maas > asgariUcret ? maas*1.1 : maas*1.5
+// console.log( `yeni maaşınız: ${maasHes}`);
+
+// //?Console'dan girilen sayilarin ortalamasini bulan programi yaziniz. Program ilk olarak kullanicidan kac tane sayi gireceginin almali bu bilgi isiginda girilen sayilarin ortalamasini hesaplayarak yazdirmalidir.
+
+// let sayac = 0;
+// let toplam = 0;
+// let n = prompt("kaç tane sayı girişi yapılacak: ");
+// while (n > sayac) {
+//   let sayi = Number(prompt("bir sayı giriniz: "));
+//   sayac++;
+//   toplam += sayi;
+// }
+// console.log(`girilen ${n} sayının ortalaması ${(toplam / sayac).toFixed(2)}`);
+
+// //? Console'dan girilen taban ve pozitif us degerlerine gore uslu sayi sonucunu hesaplayan kodu for veya while dongusu ile yaziniz.
+// let k = 0;
+// let sayı = 1;
+// let taban = +prompt("hesaplamak istediğiniz üslü sayının tabanını yazınız: ");
+// let us = +prompt("hesaplamak istediğiniz üslü sayının üssünü yazınız:");
+// while (us > k) {
+//   sayı *= taban;
+//   k++;
+// }
+// console.log(`${taban}**${us}= ${sayı}`);
+
+// //? Console'dan girilen bir sayinin basamak sayisini hesaplayarak console'a bastiran kodu donguler yardimi ile yaziniz.
+// let a = +prompt("Bir sayi giriniz")
+// let basamak= 0;
+//  for(let i=1; a>=i; i=i*10){
+//   basamak=basamak+1
+//  }
+//  console.log(basamak)
+
+// //?text = "Turkey";
+// //?Let's write a program that prints every character twice
+// //?output: TTuurrkkeeyy
+// let text = "Turkey";
+// let newText = "";
+// for (let i = 0; text.length > i; i++) {
+//   newText += text[i] + text[i];
+// }
+// console.log(newText);
+
+// //*2 yol
+// let twice = "";
+// for (let i of text) {
+//   twice += i + i;
+// }
+// console.log(twice)
+// //?0’dan 100’e kadar olan çift sayıları ekrana yazdıran programın algoritmasını yazınız.
+
+
+// //?1’den 50’ye kadar olan çift sayıların toplamını bulup ekranda gösteren programın algoritmasını yazınız.
---
+++ problemler/problemler2.js
@@ -0,0 +1,155 @@
+//? 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+//? 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+//?4 --> 0 (because 4 is already a one-digit number)
+const persistence = (num) => {
+  let multi = 1;
+  num = num + "";
+  while (num > 10) {
+    for (let i in num) {
+      console.log(num[i]);
+      multi *= num[i];
+      console.log(multi);
+    }
+    num = multi + "";
+    multi = 1;
+  }
+  return num;
+};
+
+console.log(persistence(234));
+console.log(persistence(999));
+console.log(persistence(4));
+
+//! 2 yol
+const persistence1 = (num) => {
+  let multi = 1;
+
+  while (num > 10) {
+    num = num + "";
+    num.split("").forEach((item) => (multi *= item));
+    num = multi;
+    multi = 1;
+  }
+  return num;
+};
+
+//! 3.yol
+function persistence(num) {
+  if (num.toString().length === 1) {
+    return 0;
+  }
+  let multipli = 1;
+  let splitStrng = num.toString().split("");
+  for (let i = 0; i < splitStrng.length; i++) {
+    multipli *= splitStrng[i];
+  }
+  return 1 + persistence(multipli);
+}
+
+//* Soru-1  1000 ile 2000 arasındaki 13 e bölümünden 3 kalan sayıları konsola yazdırınız ?
+// let arr = [];
+// for (let i = 1000; 2000 >= i; i++) {
+//   if (i % 13 == 3) {
+//     arr.push(i);
+//   }
+// }
+// console.log(arr);
+
+// *Soru-2  Kullanıcıdan alınan 5 sayının ortalamasını konsola yazdırınız ?
+// let toplam=0
+// for(let i=0; i< 5; i++){
+//     let num=+prompt("enter a number: ")
+//     toplam+=num
+// }
+// console.log("ort:",(toplam / 5).toFixed(2));
+
+// *Soru-3 Kullanıcıdan alınan sayıları negatif girilene kadar toplayan ve konsola yazan kodu yazınız ?
+
+// let toplam = 0;
+
+// while (true) {
+//   let num = +prompt("enter a number: ");
+//   if (num > 0) {
+//     toplam += num;
+//   }
+//   else{
+//     break
+//   }
+// }
+// console.log("toplam=", toplam)
+
+//? Soru-4 Sizden 1'den 100'e kadar sayıları yazdırmanız istenmektedir ancak 3'ün katlarına gelince 'Fizz',5'in katlarına gelince 'Buzz'.Hem 3'ün hemde 5'in katlarına gelince 'FizzBuzz' yazdırmanız istenmektedir ?
+
+// for (let i = 1; i < 100; i++) {
+// if (i % 15 == 0) {
+//     console.log("FizzBuzz");
+//   }
+//   else if (i % 3 == 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 == 0) {
+//     console.log("Buzz");
+//   }  else {
+//     console.log(i);
+//   }
+// }
+
+//? Soru-5 bir sayının(number) basamak sayısını yazdıran kodu yazınız ?
+// let count=0
+// function basamak(num){
+//     for(i of num){
+//       count+=1
+//     }
+//     return count
+// }
+// console.log(basamak("2345"));
+
+// //? soru-6 Aşağıdaki çıktıyı konsola yazdırınız ?
+// //? O
+// //? OO
+// //? OOO
+// //? OOOO
+// //? OOOOO
+// //? OOOOOO
+// //? OOOOOOO
+// let res = "";
+// for (let i = 0; i < 6; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     res += "0";
+//   }
+//   res += "\n";
+// }
+
+// console.log(res);
+
+//? soru-7 Aşağıdaki çıktıyı konsola yazdırınız ?
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// let result = ''
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= 5; j++) {
+//     result += '* '
+//   }
+//   result += '\n'
+// }
+// console.log(result)
+
+//? soru-8 Aşağıdaki çıktıyı konsola yazdırınız ?
+//    O
+//   OOO
+//  OOOOO
+// OOOOOOO
+// let str = "";
+// for (let i = 1; i <= 7; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     if (i + j >= 8) {
+//       str += "O";
+//     } else {
+//       str += " ";
+//     }
+//   }
+//   str += "\n";
+// }
+// console.log(str);
