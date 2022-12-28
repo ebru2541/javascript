e8d80726c2adf2afd11d624700bf909c43a18c74
Rose <ebrue7949@gmail.com>
2022-12-29 00:50:51 +0300

    reverse-words

--- problemler/problemler3.js
+++ problemler/problemler3.js
@@ -101,10 +101,24 @@ console.log(reverseText("Clarusway Rocks!"));
 //* ya da
 
 function reverseText1(metin) {
-    let newReverse=""
+  let newReverse = "";
   for (let i = metin.length - 1; i >= 0; i--) {
-   newReverse+=metin[i]
+    newReverse += metin[i];
   }
   return newReverse;
 }
 console.log(reverseText1("Clarusway Rocks!"));
+
+//? 9- Input : 'Clarusway' => Output : 'Clarusway'
+//?Input : 'days. big make things Little' => Output : 'Little things make big days.'
+
+const reverseWords = (word) => {
+  let words = word.split(" ");
+  if (words.length >= 2) {
+    return words.reverse().join(" ");
+  } else {
+    return word
+  }
+};
+console.log(reverseWords("Clarusway"));
+console.log(reverseWords("days. big make things Little"));
